d7e02c1b2102f23bd0da52f71f454d1a13bba75d
nareshdemo <npmcse076@gmail.com>
2024-09-22 15:29:44 +0530

    defect fixed -5678 fixed null values not to given in database

--- src/main/java/com/git_example4/A.java
+++ src/main/java/com/git_example4/A.java
@@ -1,4 +1,9 @@
 package com.git_example4;
 
+import org.springframework.boot.autoconfigure.gson.GsonProperties;
+
 public class A {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
---
+++ src/main/java/com/git_example4/B.java
@@ -0,0 +1,8 @@
+package com.git_example4;
+
+public class B {
+    public void test(){
+        System.out.println(200);
+
+    }
+}
